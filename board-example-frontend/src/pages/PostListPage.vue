099946295a30ab26a051ed833718352b12910822
kimys260@naver.com <kimys260@naver.com>
2021-01-07 17:30:46 +0900

    first commit

---
+++ board-example-frontend/src/pages/PostListPage.vue
@@ -0,0 +1,39 @@
+<template>
+    <post-list :posts="posts"/>
+</template>
+
+<script>
+import PostList from '@/components/PostList';
+import api from '@/api';
+import { mapState, mapActions } from 'vuex';
+
+export default{
+    name: 'PostListPage',
+    components:{
+        PostList
+    },
+    // data() {
+    //     return {
+    //         posts: []
+    //     }
+    // },
+    created() {
+        // api.get('/posts')
+        // .then(res => {
+        //     this.posts = res.data;
+        // })
+        this.fetchPostList();
+    },
+    computed: {
+        ...mapState([
+            'posts'
+        ])        
+    },
+    methods: {
+      ...mapActions([
+        'fetchPostList',
+        ]),
+    }
+
+}
+</script>
