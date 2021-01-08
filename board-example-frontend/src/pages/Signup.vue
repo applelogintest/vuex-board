58b7c329d793c3e23ea9a1f1f887cb6a83b60cff
kimys260@naver.com <kimys260@naver.com>
2021-01-08 09:52:36 +0900

    게시판 상세보기 연결

---
+++ board-example-frontend/src/pages/Signup.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="sign-up-page">
+        <h3>회원가입</h3>
+        <signup-form @submit="onSubmit"/>
+        <p>이미 가입하셨나요? <router-link :to="{name: 'Signup'}">로그인하러 가기</router-link></p>
+    </div>
+</template>
+
+<script>
+import SignupForm from '@/components/SignupForm';
+
+export default{
+  components: { SignupForm },
+    name: 'Signup',
+    componets: {
+        SignupForm
+    },
+    methods:{
+        onSubmit (payload) {
+            console.log(payload);
+        }
+    }
+}
+</script>
