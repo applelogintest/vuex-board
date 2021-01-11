b63e3b5b67e43ecf30137312b388f05fa32de14c
kimys260@naver.com <kimys260@naver.com>
2021-01-11 16:00:14 +0900

    게시판 댓글 CRUD 추가

--- board-example-frontend/src/pages/PostViewPage.vue
+++ board-example-frontend/src/pages/PostViewPage.vue
@@ -10,18 +10,26 @@
         <post-view v-if="post" :post="post"/>
         <p v-else> 게시글 불러오는 중..</p>
         <router-link :to="{ name: 'PostEditPage', params: { postId }}">수정</router-link>
+        <button @click="onDelete">삭제</button>
         <router-link :to="{ name: 'PostListPage'}">목록</router-link>  
+        <comment-list v-if="post" :comments="post.comments"/>
+        <comment-form @submit="onCommentSubmit"/>
     </div>
 </template>
 
 <script>
-import { mapState,mapActions } from 'vuex';
+import { mapState,mapActions,mapGetters } from 'vuex';
+import api from '@/api';
 import PostView from '@/components/PostView';
+import CommentList from '@/components/CommentList';
+import CommentForm from '@/components/CommentForm';
 
 export default{
     name: 'PostViewPage',
     components: {
-        PostView
+        PostView,
+        CommentList,
+        CommentForm
     },
     props: {
         postId: {
@@ -35,11 +43,45 @@ export default{
     computed: {
         ...mapState([
             'post'
+        ]),
+        ...mapGetters([
+            'isAuthorized'
         ])
     },
     methods: {
+        onDelete() {
+            const { id } = this.post;
+            api.delete(`/posts/${id}`)
+            .then( res => {
+                alert('게시물이 성공적으로 삭제되었습니다.');
+                this.$router.push({ name: 'PostListPage'});
+            })
+            .catch(err => {
+                if(err.response.status === 401){
+                    alert('로그인이 필요합니다.');
+                    this.$router.push({ name : 'Signin' });
+                } else {
+                    alert(err.response.data.msg);
+                }
+            })
+        },
+        onCommentSubmit(comment){
+            if( !this.isAuthorized ){
+                alert('로그인이 필요합니다');
+                this.$router.push({ name : 'Signin' });
+            } else {
+                this.createComment(comment)
+                .then( res => {
+                    alert('댓글이 성공적으로 작성되었습니다.');
+                })
+                .catch(err => {
+                    alert(err.response.data.msg);
+                })
+            }
+        },
         ...mapActions([
-            'fetchPost'
+            'fetchPost',
+            'createComment'
         ])
     },
 }
--- board-example-frontend/src/store/actions.js
+++ board-example-frontend/src/store/actions.js
@@ -1,5 +1,15 @@
 import api from '@/api';
-import { FETCH_POST_LIST, FETCH_POST, SET_ACCESS_TOKEN, SET_MY_INFO, DESTROY_ACCESS_TOKEN, DESTROY_MY_INFO } from './mutations-types';
+import {
+    FETCH_POST_LIST,
+    FETCH_POST,
+    SET_ACCESS_TOKEN,
+    SET_MY_INFO,
+    DESTROY_ACCESS_TOKEN,
+    DESTROY_MY_INFO,
+    UPDATE_COMMENT,
+    EDIT_COMMENT,
+    DELETE_COMMENT
+} from './mutations-types';
 
 export default {
     fetchPostList ({ commit }) {
@@ -42,5 +52,29 @@ export default {
     signout ({ commit }) {
         commit(DESTROY_ACCESS_TOKEN);
         commit(DESTROY_MY_INFO);
+    },
+    createComment ({ commit, state }, comment) {
+        const postId = state.post.id;
+        return api.post(`/posts/${postId}/comments`, { contents: comment })
+            .then(res => {
+                commit(UPDATE_COMMENT, res.data);
+            });
+    },
+    editComment ({ commit, state }, { commentId, comment }) {
+        const postId = state.post.id;
+        return api.put(`/posts/${postId}/comments/${commentId}`, {
+            contents: comment
+        })
+            .then(res => {
+                commit(EDIT_COMMENT, res.data);
+            });
+    },
+    deleteComment ({ commit, state }, commentId) {
+        const postId = state.post.id;
+        console.log(commentId);
+        return api.delete(`/posts/${postId}/comments/${commentId}`)
+            .then(res => {
+                commit(DELETE_COMMENT, commentId);
+            });
     }
 }
